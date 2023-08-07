25dbbd1bc9b2581a02da93a0fbdd4cf27ef5cec5
s-wasim <s.w150700@hotmail.com>
2023-08-07 12:27:00 +0500

    Fixed errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.header("Breakfast Menu")
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avacado Toast')
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
@@ -15,7 +15,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 fruits_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 fruits_list.set_index('Fruit')
 
-selected_fruits = streamlit.multiselect("Select some fruits: ", list(fruits_list['Fruit']), ['Avacado', 'Strawberries'])
+selected_fruits = streamlit.multiselect("Select some fruits: ", list(fruits_list['Fruit']), ['Avocado', 'Strawberries'])
 fruits_to_show = fruits_list.loc[selected_fruits]
 
 streamlit.dataframe(fruits_to_show)

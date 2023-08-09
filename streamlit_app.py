c94f64926a0711b476cf325734834e3cbe44d7ff
s-wasim <s.w150700@hotmail.com>
2023-08-09 12:54:14 +0500

    cleaned

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,16 +21,21 @@ fruits_to_show = fruits_list.loc[list(selected_fruits)]
 streamlit.dataframe(fruits_to_show if len(fruits_to_show) > 0 else fruits_list)
 
 streamlit.header('Fruityvice Fruit Advice!')
-# Appending data to SF
-new_fruit = streamlit.text_input(label='Add fruits: ')
-response_advice = requests.get('https://www.fruityvice.com/api/fruit/watermelon')
+advice_fruit = streamlit.multiselect('What food would you like information about?: ', list(fruits_list['Fruit']))
+response_advice = requests.get(f'https://www.fruityvice.com/api/fruit/{advice_fruit}')
 streamlit.dataframe(pd.json_normalize(response_advice.json()).set_index('name'))
 
 
+streamlit.stop()
 # Getting data from SF
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute(f"INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST VALUES ('{new_fruit}')")
 my_cur.execute('SELECT FRUIT_NAME FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST')
 res_set = my_cur.fetchall()
+
+# Appending data to SF
+new_fruit = streamlit.text_input(label='Add fruits: ')
+if len(new_fruit) > 0:
+    my_cur.execute(f"INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST VALUES ('{new_fruit}')")
+
 streamlit.dataframe(res_set)
